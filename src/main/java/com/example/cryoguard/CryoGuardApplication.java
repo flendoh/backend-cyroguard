81dd19d01185c25f6d806280f055706cd8c6cca2
mhallasi <flenditomlgcreations@gmail.com>
2026-05-14 14:53:16 -0500

    build: initialize cryoguard spring boot project

---
+++ src/main/java/com/example/cryoguard/CryoGuardApplication.java
@@ -0,0 +1,13 @@
+package com.example.cryoguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryoGuardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryoGuardApplication.class, args);
+	}
+
+}
